373b116b50322633d1aca9a22c5bb05e66157d6e
ThalerG <thalergabriel@gmail.com>
2025-06-04 11:27:48 -0300

    fix: fix headers when appending to csv

--- test_montecarlo_convergence.py
+++ test_montecarlo_convergence.py
@@ -18,14 +18,12 @@ if __name__ == '__main__':
 
     n_montecarlo = [1e3, 2e3, 5e3, 1e4, 2e4, 5e4, 1e5, 2e5, 5e5, 1e6]
 
-    results = []
-
     for N in tqdm(n_montecarlo, desc='Monte Carlo Convergence', position=0, leave=False):
         for i in tqdm(range(n_tests), desc='Test', position=1, leave=False):
             tAll, _, _, _ = res_montecarlo_temp_montecarlo(N_montecarlo = int(N))
             tmonte = {'mean_R2': np.mean(tAll['R2']), 'mean_T2': np.mean(tAll['T2']),
                       'std_R2': np.std(tAll['R2']), 'std_T2': np.std(tAll['T2']),
-                      'time_elapsed': tAll['time_elapsed'], 'N': N} 
+                      'time_elapsed': tAll['time_elapsed'], 'N': int(N)} 
             
-            pd.DataFrame([tmonte]).to_csv(saveFile, index=False, mode='a')
+            pd.DataFrame([tmonte]).to_csv(saveFile, index=False, mode='a', header=not os.path.exists(saveFile))
             
