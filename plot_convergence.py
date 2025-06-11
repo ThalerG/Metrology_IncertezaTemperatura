5dd24dc21d8ce358ceba57ab4900b1fecffca9db
ThalerG <thalergabriel@gmail.com>
2025-06-11 14:38:54 -0300

    chore: fix convergence plot size and font

--- plot_convergence.py
+++ plot_convergence.py
@@ -13,8 +13,12 @@ grouped = df.groupby(['N']).agg({
 # Flatten the MultiIndex columns
 grouped.columns = ['N', 'mean_T2_mean', 'mean_T2_std', 'std_T2_mean', 'std_T2_std']
 
+cm = 1/2.54  # centimeters in inches
+
 # Step 3: Plotting
-fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(14*cm, 12*cm))
+
+plt.rcParams["font.family"] = "Times New Roman"
 
 # Plot mean_T2 with shaded area for std
 # monte carlo T2
@@ -22,21 +26,33 @@ axes[0].plot(grouped['N'], grouped['mean_T2_mean'], label = "Monte Carlo")
 axes[0].fill_between(grouped['N'], grouped['mean_T2_mean'] - grouped['mean_T2_std'], grouped['mean_T2_mean'] + grouped['mean_T2_std'], alpha=0.2)
 
 axes[0].set_xscale('log')
-axes[0].set_xlabel('Number of Monte Carlo simulations')
-axes[0].set_ylabel('Temperature [°C]')
-axes[0].set_title('Temperature at end of test')
+axes[0].set_xlabel('Number of Monte Carlo simulations', fontname='Times New Roman')
+axes[0].set_ylabel('Temperature [°C]', fontname='Times New Roman')
 axes[0].legend(["Monte Carlo mean", "Monte Carlo standard deviation"])
+axes[0].grid(True)
+
+for label in axes[0].get_xticklabels():
+    label.set_fontproperties('Times New Roman')
+
+for label in axes[0].get_yticklabels():
+    label.set_fontproperties('Times New Roman')
 
 # monte carlo T2
 axes[1].plot(grouped['N'], grouped['std_T2_mean'], label = "Monte Carlo")
 axes[1].fill_between(grouped['N'], grouped['std_T2_mean'] - grouped['std_T2_std'], grouped['std_T2_mean'] + grouped['std_T2_std'], alpha=0.2)
 
 axes[1].set_xscale('log')
-axes[1].set_xlabel('Number of Monte Carlo simulations')
-axes[1].set_ylabel('Temperature uncertainty [°C]')
-axes[1].set_title('Temperature uncertainty at end of test')
-axes[1].legend(["Monte Carlo mean", "Monte Carlo standard deviation"])
+axes[1].set_xlabel('Number of Monte Carlo simulations', fontname='Times New Roman')
+axes[1].set_ylabel('Uncertainty [°C]', fontname='Times New Roman')
+
+axes[1].grid(True)
+
+for label in axes[1].get_xticklabels():
+    label.set_fontproperties('Times New Roman')
+
+for label in axes[1].get_yticklabels():
+    label.set_fontproperties('Times New Roman')
 
 plt.tight_layout()
-plt.savefig('Resultados/convergence_plot.pdf')
+plt.savefig('Gráficos/convergence_plot.pdf')
 plt.show()
